9a1f7909dcaca8192beb439ebd9bfd2fc2276a15
rafawang2 <rafawcy@gmail.com>
2025-03-18 22:12:41 +0800

    36th training for Resnet

--- arg.py
+++ arg.py
@@ -23,8 +23,8 @@ args_CNN = {
 }
 
 args_Res = {
-    'num_of_generate_data_for_train': 1500,
-    'epochs': 128,
+    'num_of_generate_data_for_train': 1024,
+    'epochs': 64,
     'batch_size': batch_size,
     'verbose': True,
     'type': 0,
